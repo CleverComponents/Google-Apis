9da4c6d70d96f337c7d7b4c2128285afbc4660e0
sergey <shirokov@clevercomponents.com>
2022-04-27 23:34:39 +0300

    Added Google Service Api and Calendar Api, with tests.

---
+++ test/Test.dpr
@@ -0,0 +1,31 @@
+program Test;
+{
+
+  Delphi DUnit Test Project
+  -------------------------
+  This project contains the DUnit test framework and the GUI/Console test runners.
+  Add "CONSOLE_TESTRUNNER" to the conditional defines entry in the project options
+  to use the console test runner.  Otherwise the GUI test runner will be used by
+  default.
+
+}
+
+{$IFDEF CONSOLE_TESTRUNNER}
+{$APPTYPE CONSOLE}
+{$ENDIF}
+
+uses
+  DUnitTestRunner,
+  GoogleApis.Tests in 'core\GoogleApis.Tests.pas',
+  GoogleApis in '..\src\core\GoogleApis.pas',
+  GoogleApis.Persister in '..\src\core\GoogleApis.Persister.pas',
+  GoogleApis.Calendar.Data in '..\src\calendar\GoogleApis.Calendar.Data.pas',
+  GoogleApis.Calendar in '..\src\calendar\GoogleApis.Calendar.pas',
+  GoogleApis.Calendar.Tests in 'calendar\GoogleApis.Calendar.Tests.pas';
+
+{$R *.RES}
+
+begin
+  DUnitTestRunner.RunRegisteredTests;
+end.
+
